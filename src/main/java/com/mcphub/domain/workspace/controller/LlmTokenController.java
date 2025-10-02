eef3ce1db0daf238d29f3466303439ed7f6f8e65
kwjdnjs <kwjdnjs@gmail.com>
2025-10-02 10:44:05 +0900

    [chore] swagger 수정

--- src/main/java/com/mcphub/domain/workspace/controller/LlmTokenController.java
+++ src/main/java/com/mcphub/domain/workspace/controller/LlmTokenController.java
@@ -10,6 +10,8 @@ import com.mcphub.global.common.base.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,10 +27,8 @@ public class LlmTokenController {
 
     @Operation(summary = "사용자 LLM Token을 불러오는 API", description = "사용자의 LLM Token을 불러오는 API 입니다")
     @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "LLM Token 불러오기 성공"
-            )
+            @ApiResponse(responseCode = "200", description = "LLM Token 불러오기 성공", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "400", description = "등록되지 않은 토큰에 접근", content = @Content(schema = @Schema(implementation = BaseResponse.class)))
     })
     @GetMapping(path = "/token/{llmId}")
     public BaseResponse<LlmTokenResponse> getToken(
@@ -37,16 +37,14 @@ public class LlmTokenController {
         return BaseResponse.onSuccess(llmTokenAdviser.getToken(llmId));
     }
 
-    @Operation(summary = "사용자 LLM Token 입력 API", description = "사용자의 LLM Token을 입력받아 저장하는 API 입니다")
+    @Operation(summary = "사용자 LLM Token 신규 입력 API", description = "사용자의 LLM Token을 입력받아 저장하는 API 입니다")
     @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "LLM Token 저장 성공"
-            )
+            @ApiResponse(responseCode = "200", description = "LLM Token 저장 성공", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 LLM 토큰", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "409", description = "이미 토큰이 존재",content = @Content(schema = @Schema(implementation = BaseResponse.class)))
     })
     @Parameters({
             @Parameter(name = "llmId", description = "LLM의 ID 값"),
-            @Parameter(name = "llmToken", description = "사용자의 LLM Token값"),
     })
     @PostMapping(path = "/token/{llmId}")
     public BaseResponse<LlmTokenSaveResponse> registerToken(
@@ -59,14 +57,11 @@ public class LlmTokenController {
 
     @Operation(summary = "사용자 LLM Token 수정 API", description = "사용자의 LLM Token을 입력받아 수정하는 API 입니다")
     @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "LLM Token 수정 성공"
-            )
+            @ApiResponse(responseCode = "200", description = "LLM Token 수정 성공", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 LLM 토큰, 이미 존재하는 LLM 토큰", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
     })
     @Parameters({
             @Parameter(name = "llmId", description = "LLM의 ID 값"),
-            @Parameter(name = "llmToken", description = "사용자의 LLM Token값"),
     })
     @PatchMapping(path = "/token/{llmId}")
     public BaseResponse<LlmTokenSaveResponse> updateToken(
